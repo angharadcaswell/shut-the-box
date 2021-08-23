5ff479a2c2cb0679f87c0da7aea9c7f121bf93e0
Angharad Caswell <ange.caswell1@gmail.com>
2021-08-23 15:54:26 +0000

    add input boxes for user answer and start functions to check answer is correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,47 @@
 
 
+// // Wait for the DOM to finish loading before running the game
+// document.addEventListener("DOMContentLoaded",runGame);
+
+
+
+
+// When a player selects a card, sum the cards to then be able to check against the dice total. 
+
+// let cards =document.getElementsByClassName('card');
+
+
+// function cardSelect() {
+//     this.style.backgroundColor= "red";
+//     let cardValue= (this.textContent);
+//     console.log(cardValue);
+ 
+// }
+
+// // for loop for click events:
+// for (let i=0; i < cards.length; i++) {
+//     cards[i].addEventListener('click', cardSelect);}
+
+// function createArray(){
+//     cardArray =[];
+//     cardArray.push(cardValue);
+//     console.log(cardArray);}
+
+// disable card once you've clicked roll again (or submit) and cards match the dice.
+
+function cardDisable() {
+
+}
+
+
+
+
+
+
 // change dice image to random calcualted number.
 
 function rollDice() {
+
     let diceOneImg = document.getElementById('dice-one');
     let diceTwoImg = document.getElementById('dice-two');
 
@@ -13,15 +52,43 @@ function rollDice() {
     diceOneImg.src =`assets/images/dice${diceOne}.png`;
     diceTwoImg.src =`assets/images/dice${diceTwo}.png`;
 
-    console.log(diceOne);
-    console.log(diceTwo);
-    console.log(diceTwoImg.src);
     console.log(diceTotal);
    
 
 }
 
 
-let diceInstructions = document.getElementById('dice-area');
-
+let diceInstructions = document.getElementById('dice-instructions');
 diceInstructions.addEventListener('click', rollDice);
+
+
+
+
+
+
+// Get input numbers from users and check answer against dice total: 
+
+
+function userAnswer(){
+    let answerOne = parseInt(document.getElementById('answer-one').value);
+    let answerTwo = parseInt(document.getElementById('answer-two').value);
+    let answerThree = parseInt(document.getElementById('answer-three').value);
+    let answer= answerOne + answerTwo + answerThree;
+    
+
+    let diceAnswer = rollDice();
+
+    let isCorrect = answer === diceAnswer;
+
+    if (isCorrect) {
+        alert("Correct!");
+    } else {
+        alert ("Wrong");
+    }
+    console.log(answer);
+}
+
+let submitAnswer = document.getElementById('submit-answer');
+submitAnswer.addEventListener('click', userAnswer);
+
+
